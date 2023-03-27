82e57008d29fb705fd73efae6d5a9b631116b30f
Vít Ropek <Ropek56>
2023-03-27 11:01:19 +0200

    UpdatedQuery

--- src/main/java/cz/ys/workshop/person/PersonRepository.java
+++ src/main/java/cz/ys/workshop/person/PersonRepository.java
@@ -6,9 +6,13 @@ import cz.ys.workshop.person.model.PersonEntity;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PersonRepository extends JpaRepository<PersonEntity, String> {
 	
 	public List<PersonEntity> getPersonByDepartment(DepartmentEntity department);
+	
+	@Query("SELECT a, b FROM Person a JOIN a.department b")
+	public List<PersonEntity> findAll();
 
 }
